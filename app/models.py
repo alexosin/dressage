d4994e9be629c65e190ac033909ae246b8f05da6
Alexander Osin <alexandre.osin@gmail.com>
2017-03-26 17:19:02 +0300

    Finished core

--- app/models.py
+++ app/models.py
@@ -1,35 +1,98 @@
 from app import db
 from sqlalchemy import Column, Integer, String, ForeignKey, Text, Boolean
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy_utils import ChoiceType
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import func
+
 
 
 class Chapter(db.Model):
     __tablename__ = 'chapters'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    title = Column(String, unique=True, nullable=False)
     text = Column(Text, nullable=False)
     question = relationship('Question', backref='chapter', cascade='save-update, delete')
 
-    def __str__(self):
+    @classmethod
+    def max_id(cls):
+        return db.session.query(func.max(Chapter.id)).first()
+
+    def __repr__(self):
         return '<Chapter(id {}, text {})>'.format(self.id, self.text[:10])
 
 
 class Question(db.Model):
+    TYPES = [
+        ('radio', 'radio'),
+        ('checkbox', 'checkbox')
+    ]
+
     __tablename__ = 'questions'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
     text = Column(String, nullable=False)
     chapter_id = Column(Integer, ForeignKey('chapters.id'), nullable=False)
+    type = Column(ChoiceType(TYPES), nullable=False)
     answer = relationship('Answer', backref='question', cascade='save-update, delete')
+    mark_id = Column(Integer, ForeignKey('marks.id'), nullable=False)
+
+    @classmethod
+    def max_id(cls):
+        return db.session.query(func.max(Question.id)).first()
+
+    @property
+    def correct_answers(self):
+        return db.session.query(Answer.id).filter(Answer.question_id == self.id)\
+                .filter(Answer.status).all()
+
+    @property
+    def answers(self):
+        return Answer.query.filter(Answer.question_id == self.id).order_by(func.random()).all()
+
+    def is_correct_answer(self, user_answers):
+        corrects = [i[0] for i in self.correct_answers]
+        answers = list(map(int, user_answers))
+        if self.type == 'radio':
+            return 1 if answers[0] in corrects else 0
+        elif self.type == 'checkbox':
+            count_correct_answers = len(corrects)
+            count_user_answers = 0
+            for answer in answers:
+                if answer in corrects:
+                    count_user_answers += 1
+            return count_user_answers * 100 / count_correct_answers / 100
+
+    def __str__(self):
+        return '<Question(id {}, chapter {})>'.format(self.id, self.chapter_id)
 
 
 class Answer(db.Model):
     __tablename__ = 'answers'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
     text = Column(String, nullable=False)
     status = Column(Boolean, default=False)
     question_id = Column(Integer, ForeignKey('questions.id'), nullable=False)
 
+    @classmethod
+    def count(cls):
+        db.session.query(func.count(Answer.id))
+
+    def __str__(self):
+        return '<Answer(id {}, question_id {}, status {})>'.format(
+            self.id, self.question_id, self.status
+        )
 
 
+class Mark(db.Model):
+    __tablename__ = 'marks'
+
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    mark = Column(Integer)
+    question = relationship('Question', backref='mark')
+
+    def __str__(self):
+        return '<Mark(id {}, mark {})>'.format(
+            self.id, self.mark
+        )
--- app/views.py
+++ app/views.py
@@ -1,6 +1,53 @@
-from app import app
+from app import app, db
+from flask import render_template, redirect, request, flash, url_for, session
+from .models import Chapter, Question, Answer
 
 
 @app.route('/')
 def main():
-    return 'Main page'
+    session['id'] = 0
+    session['max'] = Question.max_id()[0]
+    session['chapters'] = Chapter.max_id()[0]
+    session['mark'] = 0
+    session['rights'] = 0
+    return render_template('start.html')
+
+
+@app.route('/chapter', methods=['GET', 'POST'])
+def chapter():
+    if request.method == 'GET':
+        if 'id' in session:
+            session['id'] += 1
+            chapter = Chapter.query.filter(Chapter.id == session['id']).first()
+            return render_template('chapter.html', chapter=chapter)
+        else:
+            return redirect(url_for('main'))
+    else:
+        return redirect(url_for('quiz'))
+
+
+@app.route('/quiz', methods=['GET', 'POST'])
+def quiz():
+    if request.method == 'GET':
+        questions = Question.query.filter(Question.chapter_id == session['id']).all()
+        return render_template('quiz.html', questions=questions)
+    else:
+        forms = request.form
+        for key in forms:
+            values = forms.getlist(key)
+            question = Question.query.filter(Question.id == key).first()
+            print(values, question.is_correct_answer(values))
+            multiplier = question.is_correct_answer(values)
+            session['mark'] += 5 * multiplier
+            session['rights'] += int(multiplier)
+        if session['id'] == session['chapters']:
+            return redirect(url_for('result'))
+        return redirect(url_for('chapter'))
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.method == 'GET':
+        return render_template('result.html')
+    else:
+        return redirect(url_for('main'))
